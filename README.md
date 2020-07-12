abf8bfef6d6dc1d6d4e194e58e0078c633cc6c82
mohamed-zezo <net_66@yahoo.com>
2020-07-12 13:40:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Telecomegypt Conky
+
+<div align="center"><img src="Telecomegypt.png" width="150"></div>
+
+The Telecomegypt Conky uses the [ispapi](https://github.com/grimpy/ispapi) to get data from http://my.te.eg to show remaining quota on your desktop.
+
+## Dependencies
+
+The only dependencies are `ispapi` and `jq`.
+
+You can install them via
+
+```bash
+$ git clone https://github.com/grimpy/ispapi.git
+$ cd ispapi
+$ sudo python3 setup.py install --record files.txt
+```
+
+```bash
+$ sudo apt install jq
+```  
+
+to uninstall `ispapi`  
+```bash
+$ cd ispapi
+$ xargs rm -rf < files.txt
+```
+
+## Install
+
+```bash
+$ mkdir -p ~/.conky
+$ git clone https://github.com/mohamed_zezo/telecomegyptConky.git ~/.conky/
+```
+
+## Configuration
+
+The configuration is made via the `config.json`.
+
+```json
+{
+	"cache_dir": "/tmp/conky",
+	"phone_number": "",
+	"password": ""
+}
+```
+
+##### cache_dir
+
+This is where the telecomegyptForecast stores the temporary data.
+
+##### phone_number
+
+the phone number that used to register you account in http://my.te.eg.
+
+##### password
+
+the hashed password that can be found in the body of login post request after successful login.
+
+## How to run
+
+Once everything is configured you can run it easily:
+
+```bash
+$ cd ~/.conky/telecomegyptConky
+$ conky -c Telecomegypt
+```
+
+## Special thanks to
+
+- [@grimpy](https://github.com/grimpy) for his [ispapi](https://github.com/grimpy/ispapi) package
---
+++ telecomegyptForecast.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+#
+
+# tools required
+JQ=`which jq`
+
+if [[ -z "${JQ}" ]]; then
+  echo "ERROR: jq is not installed."
+  exit -1
+fi
+
+while getopts ":d:c:" opt; do
+  case $opt in
+    c) CACHE_DIR="${OPTARG}" ;;
+    d) DATATYPE="${OPTARG}" ;;
+    \?) echo "Invalid option -${OPTARG}" >&2 ;;
+  esac
+done
+
+if [[ -z "${CONFIG}" ]]; then
+  CONFIG="config.json"
+fi
+
+CACHE_DIR=${CACHE_DIR:-$(cat ${CONFIG} | jq '.cache_dir' -r)}
+PHONE_NUMBER=${PHONE_NUMBER:-$(cat ${CONFIG} | jq '.phone_number' -r)}
+PASSWORD=${PASSWORD:-$(cat ${CONFIG} | jq '.password' -r)}
+
+function get_cache_path() {
+  echo ${CACHE_DIR}/output.cache
+}
+
+#######################################
+# Check the cache file and determine
+# if it needs to be updated or not
+# Returns:
+#   0 or 1
+#######################################
+function cache_needs_refresh() {
+  cache_path=$(get_cache_path)
+  now=$(date +%s)
+
+  # if the cache does not exist, refresh it
+  if [[ ! -f "${cache_path}" ]]; then
+    return 1
+  fi
+
+  last_modification_date=$(stat -c %Y ${cache_path})
+  seconds=$(expr ${now} - ${last_modification_date})
+
+  if [[ "${seconds}" -gt 120 ]]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+function fetch_telecomegypt() {
+  cache_needs_refresh
+  refresh=$?
+  command=`ispapi telecomegypt -u ${PHONE_NUMBER} -p ${PASSWORD}`
+  cache_path=$(get_cache_path)
+  if [[ ! -f "${cache_path}" ]] || [[ "${refresh}" -eq 1 ]]; then
+    echo "$command" > ${cache_path}.$$
+    # only update the file if we successfully retrieved the JSON
+    num_of_lines=$(cat "${cache_path}.$$" | wc -c) #print the byte count of the file
+    if [[ "${num_of_lines}" -gt 5 ]]; then
+      mv "${cache_path}.$$" ${cache_path}
+    else
+      rm -f ${cache_path}.$$
+    fi
+  fi
+}
+
+
+function create_cache() {
+  if [[ ! -d "${CACHE_DIR}" ]]; then
+    mkdir -p ${CACHE_DIR}
+  fi
+}
+
+function return_field() {
+
+  if [[ -z "${DATATYPE}" ]]; then
+    echo "ERROR: missing datatype. Please provide it via the -d option."
+  fi
+
+  cache_path=$(get_cache_path)
+
+  case ${DATATYPE} in
+    quota)
+      data=$(awk -F',' '{print $1}' cache/output.cache)
+      ;;
+    percent)
+      data=$(awk -F',' '{printf "%.2f\n", $2}' cache/output.cache)
+      ;;
+    due)
+      data=$(awk -F',' '{print $3}' cache/output.cache)
+      ;;
+    *)
+      data="N/A"
+      ;;
+    esac
+
+  echo $data
+}
+
+create_cache
+fetch_telecomegypt
+return_field
